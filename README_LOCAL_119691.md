aafb71257e361995a2cf273e5d890edd2f25721c
591594 <trace.boggs05@gmail.com>
2026-09-25 19:52:15 +0000

    init commit. skeleton robot.py and readme

---
+++ README_LOCAL_119691.md
@@ -0,0 +1,2 @@
+ROS Version: ROS Jazzy
+Gazebo Sim Version: 8.15.0
---
+++ src/reactive_navigation/reactive_navigation/robot.py
@@ -0,0 +1,22 @@
+#Main functionality for simulated turtlebot4. Should handle movement, input,
+#   obstacle avoidance, etc.
+#PROJECT1 - Intelligent Robotics
+
+class Robot:
+    def __init__():
+        print('test')
+
+
+    #Basic robot functionality, in order of priority
+
+    def halt_on_collision():
+
+    def handle_keyboard_input():
+
+    def escape_symmetric_obstacles():
+
+    def avoid_asymmetric_obstacles():
+
+    def turn_randomly():
+
+    def drive_forward():
